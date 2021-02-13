007b64893282d85f5accb76c501a2022335d5c4d
helter daniel <helterdaniel@hotmail.com>
2021-02-13 15:13:28 -0300

    classe principal para instanciar todas as estrutura

---
+++ EstruturaDeDados/DataStructure.cs
@@ -0,0 +1,21 @@
+﻿using Estrutura.Queue;
+using Estrutura.Stack;
+using System;
+
+using System.Text;
+
+namespace Estrutura
+{
+    public class DataStructure<T>
+    {
+        public static MyStack<T> Stack()
+        {
+            return new MyStack<T>();
+        }
+
+        public static MyQueue<T> Queue()
+        {
+            return new MyQueue<T>();
+        }
+    }
+}
--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -1,4 +1,4 @@
-﻿using Estrutura.Sort;
+﻿using Estrutura;
 using System;
 
 namespace ConsoleApp1
@@ -7,22 +7,8 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            //var stack = new Stack<int>();
-            //Random randomNumber = new Random();
-            //for (int i = 0; i < 30; i++)
-            //{
-            //    stack.Push(randomNumber.Next(1, 25));
-            //}
-            //Sort sort = new Sort();
-            //var array = sort.InsertionSort(stack.ToArray());
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    Console.WriteLine(array[i]);
-            //}
-            var stack = new Stack<string>();
-            int poped;
-            stack.Pop(out poped);
-            Console.WriteLine(poped);
+            var data = DataStructure<int>.Queue();
+            data.
         }
     }
 }
